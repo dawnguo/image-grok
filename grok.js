e9ce03356792c7d27101094d68e0874a8e767407
colbyh <colbyholliday@gmail.com>
2012-10-23 18:22:02 -0700

    Fixing some semicolons and other fun stuff

--- grok.js
+++ grok.js
@@ -1,54 +1,57 @@
-if (!($ = window.jQuery)) {   
-    script = document.createElement( 'script' );  
-   	script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';  
-    script.onload=imageGrok;  
-    document.body.appendChild(script);  
-}  
+if (!($ = window.jQuery)) { //loading jQuery if it doesn't exist already
+    script = document.createElement( 'script' );
+   	script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
+    script.onload=imageGrok;
+    document.body.appendChild(script);
+}
 else {  
-    imageGrok();  
-}  
+    imageGrok();
+} 
 function imageGrok() {  
 	$(document).ready(function(){
-    	if($('#image-grabber-container').length == 0) {
+	// checking to see if the bookmarklet has been activated already
+    	if($('#iG-container').length == 0) {
 	    	$('html, body').animate({scrollTop:0}, 'fast');
-	    	$('<style type="text/css">@import url(https://www.filepicker.io/api/file/Q-EIrdPsTHqdGDkf5TgB);</style>').appendTo("head");
-	    	$('body').append('<div id="background-blocker"></div><div id="image-grabber-container"><div id="button-toggle"><span id="close">Close</span></div><ul id="list-of-images"></ul></div>');
-	    	$("#button-toggle").click(function() {
-				$("#image-grabber-container, #background-blocker").remove();
-			})
-		 	var iG = []
+		// Set this to the css file you'd like to use, feel free to edit the one here
+	    	$('<style type="text/css">@import url(http://path/to/your/css);</style>').appendTo("head");
+	    	$('body').append('<div id="background-blocker"></div><div id="iG-container"><div id="iG-close"><span id="close">Close</span></div><ul id="iG-list"></ul></div>');
+	    	$("#iG-close").click(function() {
+				$("#iG-container, #background-blocker").remove();
+			});
+		 	var iG = [];
 	    	$('img').each(function() {
+					// somewhat arbitrary numbers, just trying to keep the super small images out of the feed
+					// I've also removed .gifs from the result
 					if (this.clientWidth > 99 && this.clientHeight > 49 && this.src.indexOf('.gif') < 0){
-						iG.push(this)
+						iG.push(this);
 					}
 			});
-	    	console.log(iG.length)
-
-	    	var beginLiTag = "<li><a href='";
-			var endATag = "'>";
-			var beginImageTag = "<img src='";
-			var middleImageTag = "' style='margin-top:";
-			var endImageTag = "px'>";
-			var endLiTag = "</a></li>";
-
-
+			var iGFrameImgs = $('iframe').contents().find('img');
+			if (iGFrameImgs.length > 0) {
+				$.each(iGFrameImgs, function(){
+					if (this.clientWidth > 99 && this.clientHeight > 49 && this.src.indexOf('.gif') < 0){
+						iG.push(this);
+					}
+				});
+			}
 			$.each(iG, function(){
 				if (this.clientWidth > this.clientHeight){
-					bgSize = "background-size:auto 100%;"
+					bgSize = "background-size:auto 100%;";
 				}
 				else {
-					bgSize = "background-size:100%;"
+					bgSize = "background-size:100%;";
 				}
 	
-				finalLink = "<li style='background:url("+this.src+") center center no-repeat;"+bgSize+"'><a href='"+this.src+"'></a>"
-				$('#list-of-images').append(finalLink);
-			})
-			$('#list-of-images a').click(function(e){
+				finalLink = "<li style='background:url("+this.src+") center center no-repeat;"+bgSize+"'><a href='"+this.src+"'><span>+</span></a>";
+				$('#iG-list').append(finalLink);
+			});
+			$('#iG-list a').click(function(e){
 				e.preventDefault();
-				iGlink = "http://localhost:3000/add_image?img="+this.href
+				// in my application I use the selected image URL as an argument and pop open the corresponding link in a new window
+				iGlink = "http://path/to/your/app"+this.href;
 				window.open(iGlink,'popup','width=600,height=340');
-			})
+			});
 	    }
 	});
-};  
+}; 
 
